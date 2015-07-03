33d281a12e0724fe30ed832df638f18d98fd55e8
maickon rangel <maickonmaickon@hotmail.com>
2015-07-03 17:45:10 -0300

    criado a pagina do painel adm

--- painel.php
+++ painel.php
@@ -1,2 +1,55 @@
 <?php
-echo 'Painel PHP!';
+require_once 'init.php';
+$sessao = new Sessao();
+$login = $sessao->getVar('login');
+$pass = $sessao->getVar('senha');
+if(!isset($login) || !isset($pass)):
+	header("Location: ".BASE_PATH);
+	exit;
+endif;
+
+if(isset($_GET['logoff']) and $_GET['logoff'] == 'true'):
+	require_once 'init.php';
+	$sessao->destroy();
+	header("Location: ".BASE_PATH);
+endif;
+
+require_once 'header.php';
+	
+$parametros = array();
+$parametros['nomes']  = array('Home','Voltar','Sair');
+$parametros['links']  = array('#','#','?logoff=true');
+$parametros['programer']  = PROGRAMER;
+$parametros['copy']  = COPY;
+new Components('menu', $parametros);
+global $tag;
+
+$tag->div('class="container white"');
+	$tag->h1();
+		$tag->imprime('Painel Administrativo');
+	$tag->h1;
+	$tag->hr();
+	$tag->p();
+		$tag->smal();
+			$tag->imprime('Olá','encode');
+			$tag->b();
+				$tag->imprime($login);
+			$tag->b;
+		$tag->smal;
+	$tag->p;
+	
+	$tag->p();
+		$tag->smal();
+			$tag->imprime('Boa tarde, hoje é Sexta-feira, 03 de julho de 2015','encode');
+		$tag->smal;
+	$tag->p;
+	
+	$parametros['nomes'] 	= array(utf8_encode('Anúncios'),'Banners','Categorias','Cidades','Clientes','Galeria de Fotos','Patrocinadores','Subcategorias',utf8_encode('Vídeos'));
+	$parametros['links']  	= array(ANUNCIOPATH,BANNERPATH,CATPATH,CIDADEPARH,CLIENTEPATH,GALERIASPATH,PATROCINADORESPATH,SUBPATH,VIDEOSPATH);
+	$parametros['title']  	= array('Anúncios','Banners','Categorias','Cidades','Clientes','Galeria de Fotos','Patrocinadores','Subcategorias','Vídeos');
+	$parametros['content']  = array('Anúncios','Banners','Categorias','Cidades','Clientes','Galeria de Fotos','Patrocinadores','Subcategorias','Vídeos');
+	new Components('painel', $parametros);	
+	
+$tag->div;
+
+require_once 'footer.php';
