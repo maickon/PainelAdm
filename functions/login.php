dc159af561ff805aba8e2271a3ef5173b1bed442
maickon rangel <maickonmaickon@hotmail.com>
2015-07-03 17:43:20 -0300

    finalizado o arquivo de login

--- functions/login.php
+++ functions/login.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../init.php';
 if(isset($_GET['acao']) && $_GET['acao'] == 'login'):
 	if(isset($_POST['login'])):
 		if(empty($_POST['login'])):
@@ -18,18 +19,21 @@ if(isset($_GET['acao']) && $_GET['acao'] == 'login'):
 endif;
 
 if(!isset($erro)):
+	$connect = new Crud();
 	$login = mysql_real_escape_string(addslashes(htmlentities($_POST['login'])));
 	$senha = mysql_real_escape_string(addslashes(htmlentities($_POST['senha'])));
-	$connect = new Crud();
 	$usuario = $connect->buscar_usuario('usuario', $login, $senha);
 	if($usuario):
 		$sessao = new Sessao();
 		$sessao->setVar('login', $usuario->login_usuario);
 		$sessao->setVar('senha', $usuario->senha_usuario);
-		header("Location: {BASE_PATH.PAINEL_PATH}");
+		header("Location: ".BASE_PATH.PAINEL_PATH);
 		exit;
 	else:
 		$erro[] = 'Usuário e/ou senha inválidos.';
+		foreach($erro as $e):
+			$str .= $e.'_';
+		endforeach;
+		header("Location: ".BASE_PATH."?e={$str}");
 	endif;
 endif;
-echo 'Ola maickon!';
